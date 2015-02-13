fd90d994848a47fcb69176f899bcd997c4eae6e1
JonathanMurray <jonte_murray@hotmail.com>
2015-02-13 18:14:53 +0100

    removed commented code

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ var app = express();
 var http = require('http');
 var server = http.createServer(app);
 
-
-
-// app.set('port', (process.env.PORT || 5000));
-// app.use(express.static(__dirname + '/public'));
-
 app.get('/', function(request, response) {
   response.sendFile('index.html', {root: "./views"});
 });
