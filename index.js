bcc367d27fc201e795cf425d6301a2f09cb52506
JonathanMurray <jonte_murray@hotmail.com>
2015-02-13 18:13:04 +0100

    Added html, changed to newer conventions in server code

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 var express = require('express');
-var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+
+
+
+// app.set('port', (process.env.PORT || 5000));
+// app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.sendFile('index.html', {root: "./views"});
 });
 
-app.get('/person', function(request, response) {
-  response.send('Hello Jonathan!');
-});
-
-app.get('/person/:name', function(request, response) {
-  var name = request.param('name');
-  response.send('Hello ' + name);
-});
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
+server.listen(process.env.PORT || 5000);
+console.log('Express server started on port %s', server.address().port);
