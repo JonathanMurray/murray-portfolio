630c8ad6a7065ed39117bf94579ccb7718ee3c0f
JonathanMurray <jonte_murray@hotmail.com>
2015-02-13 17:51:37 +0100

    Changed server code to use express instead.

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function(req, res) {
-	res.writeHead(200);
-	res.end('Hello http');
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
-server.listen(8081);
-console.log('Hello listening');
+
+app.get('/person', function(request, response) {
+  response.send('Hello Jonathan!');
+});
+
+app.get('/person/:name', function(request, response) {
+  var name = request.param('name');
+  response.send('Hello ' + name);
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
