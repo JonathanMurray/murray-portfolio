54bb3e5647568ccb82d6b955388d7fa0016e4c92
JonathanMurray <jonte_murray@hotmail.com>
2015-02-14 13:01:59 +0100

    copied over code from other Flask portfolio project. Lots of changes, due to nodejs and other renderengine (handlebars)

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 var express = require('express');
+var expressHbs = require('express-handlebars');
+
+hbs = expressHbs.create({
+	extname:'html', 
+	defaultLayout:'main.html',
+	helpers: require('./handlebars-helpers')
+});
 
 var app = express();
 var http = require('http');
 var server = http.createServer(app);
 
-app.get('/', function(request, response) {
-  response.sendFile('index.html', {root: "./views"});
+app.engine('html', hbs.engine);
+app.set('view engine', 'html');
+
+tabs = [
+	{name:"about", link_text:"about"},
+	{name:"chatbot", link_text:"chatbot", demo:true, description: "A Prolog project"},
+	{name:"pong", link_text:"pong", demo:true, description: "A simple JavaScript-game"},
+	{name:"genetic-elevators", link_text:"genetics", description: "Computer Science Bachelor's project"},
+	{name:"soundcloud", link_text:"soundcloud"},
+	{name:"youtube", link_text:"youtube"},
+];
+
+app.get('/', function(req, res) {
+	res.render('main.html', {layout: false, tabs:tabs});
 });
+
+app.use('/static', express.static(__dirname + '/public'));
 
 server.listen(process.env.PORT || 5000);
 console.log('Express server started on port %s', server.address().port);
---
+++ public/js/layout.js
@@ -0,0 +1,101 @@
+
+function mod(n, m) {
+  return ((m % n) + n) % n;
+}
+
+$(document).ready(setup);
+$(document).keydown(keyHandler);
+$(document).keyup(keyHandler);
+
+function setup(){
+  var tab = getActiveTabFromUrl();
+  setActiveTab(tab);
+  $("nav a").click(navClick);
+  setupChatbot();
+  setupNavigationArrows();
+  setupPong();
+}
+
+function getActiveTabFromUrl(){
+  var activeTab = window.location.hash.substring(1);
+  if(activeTab){
+    return activeTab;
+  }
+  return TABS[0] //DEFAULT
+}
+
+function navClick(event){
+  var aTag = $(event.target).closest("a");
+  var tabName = aTag.get(0).hash.substring(1); //Substring gets rid of first char '#'
+  setActiveTab(tabName);
+}
+
+function setActiveTab(tabName){
+  window.location.href = "#" + tabName;
+  $(".active-link").removeClass("active-link");
+  $("a[href='#" + tabName + "']").addClass("active-link");
+  $(".active-tab").removeClass("active-tab");
+  $("#" + tabName + "-tab").addClass("active-tab");
+}
+
+function keyHandler(event){
+  if(event.type == "keydown"){
+    navigationKeyDown(event);
+  }
+  if(getActiveTabFromUrl() == "pong"){
+    pongKeyHandler(event);
+  }
+}
+
+function navigationKeyDown(event){
+  var indexChange = 0;
+  if(event.keyCode == 37){ // left arrow
+    indexChange = - 1;
+  }else if(event.keyCode == 39){ // right arrow
+    indexChange = 1;
+  }
+  if(indexChange != 0){
+    changeTab(indexChange);  
+  }
+}
+
+// indexChange == 1 ==> next tab
+// indexChange == -1 ==> prev. tab
+function changeTab(indexChange){
+  var currentTab = getActiveTabFromUrl();
+  var currentTabIndex = TABS.indexOf(currentTab);
+  var newTab = TABS[mod(TABS.length, currentTabIndex + indexChange)];
+  setActiveTab(newTab);  
+}
+
+function setupChatbot(){
+  $("#send-chatbot-input").click(
+    function(){
+      sendChatbotInput($("#chatbot-input").val());
+    });
+}
+
+function sendChatbotInput(input){
+  formattedInput = input.replace(" ", "+");
+  $.ajax({
+    url: "http://localhost:5001/?input=" + formattedInput,
+    context: $("#chatbot-output")
+  }).done(function(data) {
+    $( this ).html(data);
+  });
+}
+
+function setupNavigationArrows(){
+  $(".right-arrow-link").click(
+    function(){
+      changeTab(1);
+    });
+  $(".left-arrow-link").click(
+    function(){
+      changeTab(-1);
+    });
+}
+
+
+
+
